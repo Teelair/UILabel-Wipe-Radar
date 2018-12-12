dfa22903054dcd4cbff0a1e3e7eedd1e47de7908
Tyler Rife <tylerrifeasd@gmail.com>
2018-12-12 16:24:01 -0700

    Commit of project that can easily reproduce radar.

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -1,20 +1,47 @@
 //
 //  ViewController.swift
-//  Testing
+//  UILabel Wipe
 //
-//  Created by Tyler Rife on 12/12/18.
-//  Copyright © 2018 Tyler Rife. All rights reserved.
+//  Created by Tyler R on 12/12/18.
+//  Copyright © 2018 Tyler R. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var dynamicTableView: UITableView!
+    var labels: [UILabel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        dynamicTableView.delegate = self
+        dynamicTableView.dataSource = self
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        for cell in dynamicTableView.visibleCells {
+            if let textLabel = cell.textLabel {
+                labels.append(textLabel)
+            }
+        }
+        for label in labels {
+            print(label.superview as Any)
+        }
+    }
+    
+    @IBAction func listOffSuperviews(_ sender: Any) {
+        for label in labels {
+            print(label.superview as Any)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(indexPath.row)") else { return UITableViewCell() }
+        return cell
+    }
 }
-
