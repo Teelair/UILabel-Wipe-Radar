e46ca16686d35b3841d1647ed9d4e6e3ce7e32fa
Tyler Rife <tylerrifeasd@gmail.com>
2018-12-12 14:42:50 -0700

    Initial Commit

---
+++ Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Tyler Rife on 12/12/18.
+//  Copyright © 2018 Tyler Rife. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
